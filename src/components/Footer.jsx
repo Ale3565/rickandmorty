bd67541d00c139bad6ec8c9b5bda6dc2399c6ee8
Alexis Javier Levano Saire <levanosaire@gmail.com>
2022-08-09 15:47:04 -0500

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+       <p><b>Web Developer: </b>Alexis Levano</p>
+    </div>
+  )
+}
+
+export default Footer
